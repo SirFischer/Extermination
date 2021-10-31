717ad7a145ef11c25bbaa9d2baef496f9d10a9a1
Marek <marek5886@hotmail.com>
2021-10-31 11:12:46 +0100

    fixed rendering size

--- src/States/Game/World/Map/Map.cpp
+++ src/States/Game/World/Map/Map.cpp
@@ -4,7 +4,7 @@
  * File Created: Saturday, 23rd October 2021 7:33:45 pm
  * Author: Marek Fischer
  * -----
- * Last Modified: Sunday, 31st October 2021 10:56:35 am
+ * Last Modified: Sunday, 31st October 2021 11:12:22 am
  * Modified By: Marek Fischer 
  * -----
  * Copyright - 2021 Deep Vertic
@@ -38,7 +38,9 @@ void	Map::Render(Yuna::Core::Window *pWindow, const sf::View	&pView)
 {
 	std::string		lastPath = "";
 	mSprite.setScale(1, 1);
-	auto list = mBlockQTree->RangeSearch(sf::FloatRect(sf::Vector2f(pView.getCenter().x - 1000.f, pView.getCenter().y - 1000.f), sf::Vector2f(2000, 2000)));
+	auto list = mBlockQTree->RangeSearch(sf::FloatRect(
+		sf::Vector2f(pView.getCenter().x - ((pView.getSize().x / 2.f) + mGridSize) , pView.getCenter().y - ((pView.getSize().y / 2.f) + mGridSize)),
+		sf::Vector2f(pView.getSize().x + (mGridSize * 2.f), pView.getSize().y + (mGridSize * 2.f))));
 	for (auto &a : list)
 	{
 		for (auto &b : *a)
